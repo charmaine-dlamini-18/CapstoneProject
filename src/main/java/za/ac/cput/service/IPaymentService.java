f91fc2ab4cf0b0c9fe20046fe2c076ca3b3a2993
SafiyaElmi <240500598@mycput.ac.za>
2026-06-22 08:58:51 +0200

    Update IPaymentService.java

--- src/main/java/za/ac/cput/service/IPaymentService.java
+++ src/main/java/za/ac/cput/service/IPaymentService.java
@@ -1,1 +1,17 @@
+package za.ac.cput.service;
 
+import za.ac.cput.domain.Payment;
+
+import java.util.List;
+
+/*
+PaymentRepository.java
+Payment repository
+Author: Safiya Elmi
+(240500598)
+Date: 22/06/2026
+*/
+
+public interface IPaymentService extends IService<Payment, String> {
+    List<Payment> getAll();
+}
